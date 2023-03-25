fc2ec52830027d9227d9488c9c6d9a7c0550ccdc
Hamza Hajeir <hahajeir@gmail.com>
2023-03-25 17:30:25 +0300

    Adds h4plugins as a dependencee

--- lib/H4Plugins/SPIFFS/spiffs.bat
+++
@@ -1,6 +0,0 @@
-echo off
-del spiffs*.bin
-set /p v=<../data/h4UI
-echo Making version %v%
-mkspiffs -c ../data/ spiffs_%v%_1M.bin > nul
-mkspiffs -p 256 -b 8192 -s 0xFA000 -c ../data/ spiffs_%v%_4M.bin > nul
--- lib/H4Plugins/examples/YOUTUBE/YOUTUBE_GPIO1000rotary/YOUTUBE_GPIO1000rotary.ino
+++
@@ -1,7 +0,0 @@
-#include<H4Plugins.h>
-H4_USE_PLUGINS(115200,H4_Q_CAPACITY,false) 
-
-h4pEncoder rotary(D1,D2);
-
-void onGPIO(int pin,int value){ Serial.printf("P%d V%d\n",pin,value); }
-void h4pGlobalEventHandler(const std::string& svc,H4PE_TYPE t,const std::string& msg){ switch(t){ H4P_FUNCTION_ADAPTER_GPIO; } }
