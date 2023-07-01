3929836904ab5d79df36a2bafc12a9d4b67c9072
Hamza Hajeir <hahajeir@gmail.com>
2023-07-01 19:19:55 +0300

    Prepares the environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,78 @@
 #include <Arduino.h>
+#include "config.h"
 #include <H4Plugins.h>
-H4_USE_PLUGINS(115200, H4_Q_CAPACITY, false) // Serial baud rate, Q size, SerialCmd autostop
-//
-const char *WIFI_SSID = "XXXXXXXX";
-const char *WIFI_PASS = "XXXXXXXX";
-const char *MQTT_SERVER = "http://192.168.1.34:1883";
+H4_USE_PLUGINS(PROJ_BAUD_RATE, H4_Q_CAPACITY, false) // Serial baud rate, Q size, SerialCmd autostop
 //
 
-// const char* pload0="multi-line payload hex dumper which should split over several lines, with some left over";
-// const char* pload1="PAYLOAD QOS1";
-// const char* pload2="Save the Pangolin!";
 
-
-// #define BIG_SIZE 5000
-// #define MED_SIZE 500
-
-// char big[BIG_SIZE];
-// char med[MED_SIZE];
-
+H4P_SerialLogger h4sl;
 H4P_PinMachine h4gm;
-H4P_WiFi h4wifi(WIFI_SSID, WIFI_PASS, "uiinputs");
+
+#ifdef ARDUINO_ARCH_ESP8266
+std::string name = "ESP8266";
+#else
+std::string name = "ESP32";
+#endif
+
+H4P_WiFi h4wifi(WIFI_SSID, WIFI_PASS, name);
+#if USE_MQTT
 H4P_AsyncMQTT h4mqtt(MQTT_SERVER);
+#endif
 H4P_Heartbeat h4hb;
+H4P_BinarySwitch h4onoff(4, ACTIVE_LOW, H4P_UILED_ORANGE, OFF, 10000);
 H4P_UPNPServer h4upnp("UI Input Tester");
 
 void publishDevice(const std::string &topic, const std::string &payload)
 {
 	Serial.printf("Publishing %s to %s\n", CSTR(payload), CSTR(topic));
+#if USE_MQTT
 	h4mqtt.publishDevice(topic, payload, 1);
+#endif
 }
 void publishDevice(const std::string &topic, long long payload)
 {
 	publishDevice(topic, stringFromInt(payload, "%lu"));
 }
 H4_TIMER sender;
-H4_TIMER reporter;
 
-void onWiFiConnect(){
+void onWiFiConnect() {
 	Serial.printf("Connected, IP: %s\n",WiFi.localIP().toString().c_str());
 }
-void onWiFiDisconnect(){
+void onWiFiDisconnect() {
 	Serial.printf("WiFi Disconnected\n");
+#if USE_MQTT
+	h4mqtt.informNetworkState(H4AMC_NETWORK_DISCONNECTED); // [ ] Move to H4Plugins.
+#endif
 }
-void onMQTTConnect(){
-
-	sender = 	h4.every(200, []()
-			 {
-    // publishDevice("heap",_HAL_freeHeap());
-		// publishDevice("uptime",h4p.gvGetInt(upTimeTag()));
-		// publishDevice("hello",std::string("Hello from ") + h4p.gvGetstring(deviceTag()));
-		// publishDevice("millis",millis()); 
-		Serial.printf("Heap: %u\n", heap_caps_get_free_size(MALLOC_CAP_DEFAULT));
-		// auto bigtName = std::string("h4/") + (h4p[deviceTag()] + "/big");
-		// auto medtName = std::string("h4/") + (h4p[deviceTag()] + "/med");
-
-		// auto p0Topic = std::string("h4/") + (h4p[deviceTag()] + "/0");
-		// auto p1Topic = std::string("h4/") + (h4p[deviceTag()] + "/1");
-		// auto p2Topic = std::string("h4/") + (h4p[deviceTag()] + "/2");
-
-		// h4mqtt.publish(p0Topic.c_str(), pload0, strlen(pload0), 0);
-		// h4mqtt.publish(p1Topic.c_str(), pload1, strlen(pload1), 1);
-		// h4mqtt.publish(p2Topic.c_str(), pload2, strlen(pload2), 2);
-
-		// h4mqtt.publish(medtName.c_str(),med, MED_SIZE, 1, false); 
-		});
-
-	// reporter = h4.every(5000, []()
-	// 		 { publishDevice("heap", heap_caps_get_free_size(MALLOC_CAP_DEFAULT)); });
+void onMQTTConnect() {
+	sender = h4.every(2000, []()
+					  {
+						  publishDevice("heap", _HAL_freeHeap());
+						  publishDevice("uptime",h4p.gvGetInt(upTimeTag()));
+					  });
 
 }
-void onMQTTDisconnect(){
+void onMQTTDisconnect() {
 	h4.cancel(sender);
-	h4.cancel(reporter);
 }
+void onViewersConnect() {
+	h4wifi.uiAddGlobal("heap");
+	// h4wifi.uiAddGlobal("pool");
+}
+void onViewersDisconnect() {
+}
+
 void h4pGlobalEventHandler(const std::string& svc,H4PE_TYPE t,const std::string& msg)
 {
 	switch (t)
 	{
-	H4P_DEFAULT_SYSTEM_HANDLER;
+		H4P_DEFAULT_SYSTEM_HANDLER;
 	case H4PE_SERVICE:
 		H4P_SERVICE_ADAPTER(MQTT);
 		H4P_SERVICE_ADAPTER(WiFi);
+		break;
+	case H4PE_VIEWERS:
+		H4P_SERVICE_ADAPTER(Viewers);
 		break;
 	default:
 		break;
@@ -89,7 +81,36 @@ void h4pGlobalEventHandler(const std::string& svc,H4PE_TYPE t,const std::string&
 }
 void h4setup()
 {
-	// for(auto i=0;i<BIG_SIZE;i++) big[i]=i;
-	// for(auto i=0;i<MED_SIZE;i++) med[i]=i;
+#ifdef MBEDTLS_DEBUG_C
+	mbedtls_debug_set_threshold(1);
+#endif
 
+#if H4P_SECURE
+	auto mqCert = reinterpret_cast<const uint8_t*>(const_cast<char*>(MQTT_CERT.c_str()));
+	Serial.printf("MQTT CERT Validation\n");
+	H4AsyncClient::isCertValid(mqCert,MQTT_CERT.length()+1);
+	Serial.printf("WEBSERVER CERT Validation\n");
+	H4AsyncClient::isCertValid((const uint8_t*)WEBSERVER_CERT.c_str(), WEBSERVER_CERT.length() + 1);
+	Serial.printf("WEBSERVER KEY Validation\n");
+	H4AsyncClient::isPrivKeyValid((const uint8_t*)WEBSERVER_PRIV_KEY.c_str(), WEBSERVER_PRIV_KEY.length() + 1);
+
+#if USE_MQTT
+	h4mqtt.secureTLS(mqCert, MQTT_CERT.length()+1);
+#endif
+#if SECURE_WEBSERVER
+	h4wifi.hookWebserver([](){
+		h4wifi.secureTLS((const uint8_t*)WEBSERVER_PRIV_KEY.c_str(), WEBSERVER_PRIV_KEY.length() + 1, 
+							NULL, 0,
+						(const uint8_t*)WEBSERVER_CERT.c_str(), WEBSERVER_CERT.length() + 1);
+		h4wifi.useSecurePort();
+	});
+#endif // SECURE_WEBSERVER
+#endif // H4P_SECURE
+
+	h4.every(300, []()
+			 {
+				Serial.printf("H=%u M=%u S=%u\n", _HAL_freeHeap(), _HAL_maxHeapBlock(), uxTaskGetStackHighWaterMark(NULL));
+				h4p["heap"] = _HAL_freeHeap();
+				// h4p["pool"] = mbx::pool.size();
+				});
 }
