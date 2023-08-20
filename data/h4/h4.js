20524d11133c119e3eecb0b4fc573a6523126142
Hamza Hajeir <hahajeir@gmail.com>
2023-08-20 18:29:41 +0300

    Updates to 3.3.3 with WebSockets integrated

--- data/h4/h4.js
+++ data/h4/h4.js
@@ -1,34 +1,61 @@
-let source
+let ws
+let w
 let colortable=["red","orange","yellow","green","blue","bi"]
 const RECORD_SEPARATOR="|"
 const UNIT_SEPARATOR="~"
+
+class WebSocketEventManager {
+    constructor() {
+        this.listeners = {};
+        this.cbmessage = null;
+    }
+    hook(type, cb) {
+        if (!this.listeners[type]) {
+            this.listeners[type] = [];
+        }
+        this.listeners[type].push(cb);
+    }
+    unhook(type, cb) {
+        const listeners = this.listeners[type];
+        if (listeners) {
+            this.listeners[type] = listeners.filter(listener => listener !== cb);
+        }
+    }
+    onmessage(cb) {
+        this.cbmessage = cb;
+    }
+    handle(msg) {
+        const parts = msg.split(",");
+        const type = parts[0];
+        const data = parts.slice(1).join(",");
+
+        if (type === '') {
+            if (this.cbmessage) {
+                this.cbmessage(data);
+            }
+        } else {
+            this.handleEvent(type, data);
+        }
+    }
+    handleEvent(eventType, data) {
+        const listeners = this.listeners[eventType] || [];
+        listeners.forEach(listener => {
+            if (typeof listener === 'function') {
+                listener(data);
+            }
+        });
+    }
+}
 
 function toaster(t){
     msg.innerHTML=t;
     setTimeout(function () { msg.innerHTML="&nbsp;"; },30000);
 }
 
-function ajax(url,decode=true){
+function send(url){
     document.body.style.cursor = "wait";
     toaster("&nbsp;");
-    var http = new XMLHttpRequest();
-    var fullurl="http://"+window.location.hostname+"/rest/"+url;
-    http.open('GET', fullurl);
-    let r=document.getElementById("reply")
-    http.addEventListener("load", function(e){
-        document.body.style.cursor = "default";
-        if(decode){
-            r.innerHTML=""
-            r.style.color="#ffffff" // css
-            var j=JSON.parse(e.currentTarget.responseText);
-            if(j.res) toaster("Error: "+j.res+" "+j.msg)
-            else {
-                j.lines.forEach(function(l){ r.innerHTML+=l+'\n'})
-                document.querySelectorAll("input.edit").forEach(x => x.classList.remove("edit") )
-            }
-        }
-    })
-    http.send();
+    w.send(url);
 }
 
 function properCase(s){ return s.split(" ").map(function (x){ return x.charAt(0).toUpperCase() + x.slice(1) }).join(" ") }
@@ -44,11 +71,11 @@ function makeBaseValueNode(id,type,cl,f){
     let n=document.createElement(type);
     n.id=id;
     if(cl.length) n.classList=cl;
-    source.addEventListener(id, (e) => f(document.getElementById(id),e) )
+    ws.hook(id, (e) => f(document.getElementById(id),e) )
     return n;
 }
 
-function ajaxvalue(e){ ajax("h4/config/"+e.target.id+"=`"+e.target.value+"`") }
+function sendvalue(e){ send("h4/config/"+e.target.id+"=`"+e.target.value+"`") }
 
 function btnImg(n,v){ n.src="/"+n.id+parseInt(v)+".jpg" }
 
@@ -70,38 +97,38 @@ function makeValue(id,type,active,color,value){
     let imgNode;
     switch(type){
         case 0: // TEXT
-            valueNode=makeBaseValueNode(id,"div","uv",(n,e) => n.innerHTML=e.data )
+            valueNode=makeBaseValueNode(id,"div","uv",(n,e) => n.innerHTML=e )
             valueNode.innerHTML=value;
             break;
         case 1: // GPIO
         case 2: // BOOL
             let hue=colortable[color];
             valueNode=makeBaseValueNode(id,"div","ld led-"+hue+"-"+parseInt(value),(n,e) => {
-                let b=parseInt(e.data)
+                let b=parseInt(e)
                 let C="led-"+hue+"-";
                 n.classList.remove(C+(b ? 0:1));
                 n.classList.add(C+b);
             })
             break;
         case 3: // input
-            valueNode=makeBaseValueNode(id,"input","",(n,e) => { n.value=e.data })
+            valueNode=makeBaseValueNode(id,"input","",(n,e) => { n.value=e })
             valueNode.type="text";
             valueNode.value=value;
             valueNode.addEventListener("focus", (e) => e.target.classList.add("edit") )
             valueNode.addEventListener("blur",  (e) => {
                 let n=document.getElementById(e.target.id);
-                if(n.value!=value) ajaxvalue(e)
+                if(n.value!=value) sendvalue(e)
                 else n.classList.remove("edit")
             })
             break;
         case 4: // image button
-            imgnode=makeBaseValueNode(id,"input","",(n,e) => btnImg(n,e.data) )
+            imgnode=makeBaseValueNode(id,"input","",(n,e) => btnImg(n,e) )
             imgnode.type="image";
             btnImg(imgnode,value);
             imgnode.addEventListener("click", (e) => {
                 let n=document.getElementById(e.target.id);
                 let v=parseInt(n.src.replace(".jpg","").slice(-1))
-                ajax("h4/config/"+n.id+"="+(v ? 0:1))
+                send("h4/config/"+n.id+"="+(v ? 0:1))
             })
             valueNode=document.createElement("div")
             valueNode.classList="bouter"
@@ -111,9 +138,9 @@ function makeValue(id,type,active,color,value){
             valueNode.appendChild(binner)
             break;
         case 5: // dropdown
-            valueNode=makeBaseValueNode(id,"select","",(n,e) => options(n,e.data) )
+            valueNode=makeBaseValueNode(id,"select","",(n,e) => options(n,e) )
             options(valueNode,value)
-            valueNode.addEventListener("change", (e) => ajaxvalue(e) )
+            valueNode.addEventListener("change", (e) => sendvalue(e) )
             break;
         case 6: // image
             valueNode=document.createElement("div")
@@ -122,7 +149,7 @@ function makeValue(id,type,active,color,value){
             imgnode.id=id;
             imgnode.src=value;
             valueNode.appendChild(imgnode)
-            source.addEventListener(id, (e) => document.getElementById(id).src=e.data )
+            ws.hook(id, (e) => document.getElementById(id).src=e )
             break;
         default:
             valueNode=makeBaseValueNode(id,"div","uv");
@@ -140,38 +167,67 @@ function ui2(d){
         hanger.style="display: grid" // light 'em up (unused stays invisible)
         let active=parseInt(parts[3])
         let color=parts[4]
-        let value=parts[5]
+        let value=parts.slice(5).join(",")
         if(type==4 || type==6) hanger.insertBefore(makeValue(id,type,active,color,value),null) // img button = full-width
         else {
             hanger.insertBefore(makeTitle(id,active ? "tuia":""),null)
             hanger.insertBefore(makeValue(id,type,active,color,value),null)
         }
-    }
+}
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    if(!!window.EventSource) source=new EventSource("/evt");
+    if (!!window.WebSocket) { 
+        ws = new WebSocketEventManager();
+        w = new WebSocket("ws://"+document.location.host+"/ws");
+        w.onerror = function(e) {
+            console.warn(e);
+            document.querySelectorAll('div.hanger').forEach((d) => d.style.background="#ff0000")
+            toaster("Error "+e)
+        };
+        w.onopen = function() { 
+            // ws.send("Hello there!");
+            toaster("Connection open")
+        };
+        w.onmessage = function (evt) {
+            ws.handle(evt.data);
+        };
+        w.onclose = function() { toaster("Connection closed") }
+    } else { console.error("WebSocket isn't supported in this browser") }
+
     let rr=document.getElementById("reply")
     let cmd=document.getElementById("cmd")
     let msg=document.getElementById("msg")
 
-    source.addEventListener('ui', function(e){ ui2(e.data) });
-    source.onmessage=function(e){ toaster(e.data) }
-    source.onerror=function(e){ document.querySelectorAll('div.hanger').forEach((d) => d.style.background="#ff0000") }
+    ws.hook('ui', function(e){ ui2(e) });
+    ws.onmessage((e)=>{ toaster(e) });
+
+    ws.hook('reply', (resp)=>{
+        let r=document.getElementById("reply")
+        document.body.style.cursor = "default";
+        r.innerHTML=""
+        r.style.color="#ffffff" // css
+        var j=JSON.parse(resp);
+        if(j.res) toaster("Error: "+j.res+" "+j.msg)
+        else {
+            j.lines.forEach(function(l){ r.innerHTML+=l+'\n'})
+            document.querySelectorAll("input.edit").forEach(x => x.classList.remove("edit") )
+        }
+    })
 
     toaster("Thank you for using H4/Plugins - please support me on Patreon, see link below");
-        
+
     document.getElementById("cc").addEventListener('submit', function(e){
         e.preventDefault();
-        ajax(cmd.value,true);
+        send(cmd.value);
     },{capture: true});
 
     rr.addEventListener('click', function(e){
         let lines=rr.value.split("\n")
         let n=rr.value.substr(0, rr.selectionStart).split("\n").length
         cmd.value=lines[n-1]
-        ajax(lines[n-1],true)
+        send(lines[n-1])
     },{capture: true});
 
-    document.getElementById("qh").addEventListener('click', function(e){ ajax("help",true) },{capture: true});
+    document.getElementById("qh").addEventListener('click', function(e){ send("help") },{capture: true});
 })
